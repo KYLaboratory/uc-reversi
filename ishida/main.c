dece59cc0f7bfa4471f3f50ca80491a3f7f89875
Shogo Ishida <general5stone@gmail.com>
2014-06-20 21:36:25 +0900

    added handa directory

--- ishida/main.c
+++ ishida/main.c
@@ -2,7 +2,6 @@
 
 int main(void)
 {
-  //unsigned char switch_flag = 0;
   unsigned short data;
 
   ADCSRA = 0b10000100;
@@ -10,32 +9,32 @@ int main(void)
 
   DDRA |= _BV(PA2);
   PORTA &= ~_BV(PA2);
+  DDRA |= _BV(PA6);
+  PORTA &= ~_BV(PA6);
+
+  PORTA |= _BV(PA6);
+
+  unsigned int i = 0;
 
   while(1) {
-    //if (switch_flag) {
-    //ADMUX = 0b00000110;
-      ADCSRA |= _BV(ADSC);
-      loop_until_bit_is_set(ADCSRA, ADIF);
-      data = ADC;
+    /*i++;
+    if (i < 30000) {
+      PORTA |= _BV(PA6);
+    } else if (i < 60000) {
+      PORTA &= ~_BV(PA6);
+    } else {
+      i = 0;
+      }*/
 
-      if (data >= 30) {
-        PORTA |= _BV(PA2);
-      } else {
-        PORTA &= ~_BV(PA2);
-      }
-      //}
-
-    /*ADMUX = 0b00000000;
     ADCSRA |= _BV(ADSC);
     loop_until_bit_is_set(ADCSRA, ADIF);
-    data = ADC;
+    data = ADCW;
 
-    if (data >= 30) {
-      //PORTA |= _BV(PA2);
+    if (data >= 5) {
+      PORTA |= _BV(PA2);
     } else {
-      switch_flag = 1;
-      //PORTA &= ~_BV(PA2);
-      }*/
+      PORTA &= ~_BV(PA2);
+    }
   }
 
   return 0;
