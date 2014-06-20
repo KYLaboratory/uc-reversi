fba2e579671a4e85b70c42ba9672934542c027c0
Shogo Ishida <general5stone@gmail.com>
2014-06-20 17:09:32 +0900

    switch

--- ishida/main.c
+++ ishida/main.c
@@ -2,6 +2,7 @@
 
 int main(void)
 {
+  //unsigned char switch_flag = 0;
   unsigned short data;
 
   ADCSRA = 0b10000100;
@@ -11,32 +12,31 @@ int main(void)
   PORTA &= ~_BV(PA2);
 
   while(1) {
+    //if (switch_flag) {
+    //ADMUX = 0b00000110;
+      ADCSRA |= _BV(ADSC);
+      loop_until_bit_is_set(ADCSRA, ADIF);
+      data = ADC;
+
+      if (data >= 30) {
+        PORTA |= _BV(PA2);
+      } else {
+        PORTA &= ~_BV(PA2);
+      }
+      //}
+
+    /*ADMUX = 0b00000000;
     ADCSRA |= _BV(ADSC);
     loop_until_bit_is_set(ADCSRA, ADIF);
     data = ADC;
 
     if (data >= 30) {
-      PORTA |= _BV(PA2);
+      //PORTA |= _BV(PA2);
     } else {
-      PORTA &= ~_BV(PA2);
-    }
+      switch_flag = 1;
+      //PORTA &= ~_BV(PA2);
+      }*/
   }
 
   return 0;
-
-  /*
-  DDRA = 0b10000000;
-  int i = 0;
-  while (1){
-    i++;
-    if (i < 10000) {
-      PORTA = 0b10000000;
-    } else if (i < 20000) {
-      PORTA = 0b00000000;
-    } else {
-      i = 0;
-    }
-  }
-  return 0;
-  */
 }
